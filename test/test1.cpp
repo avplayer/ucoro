7cd7cd64c3c04c7808e63cd0ec54b0420051cdbd
microcai <microcaicai@gmail.com>
2024-09-30 17:24:06 +0800

    add test code

---
+++ test/test1.cpp
@@ -0,0 +1,36 @@
+boost::asio::io_context main_ioc;
+
+cppcoro::awaitable<int> coro_compute_int(int value)
+{
+	auto ret = co_await manual_awaitable<int>([value](auto handle) {
+		main_ioc.post([value, handle = std::move(handle)]() mutable {
+			std::this_thread::sleep_for(std::chrono::seconds(0));
+			std::cout << value << " value\n";
+			handle(value * 100);
+		});
+	});
+
+	co_return (value + ret);
+}
+
+cppcoro::awaitable<void> coro_compute_exec(int value)
+{
+	auto ret = co_await coro_compute_int(value);
+	std::cout << "return: " << ret << std::endl;
+	co_return;
+}
+
+cppcoro::awaitable<void> coro_compute()
+{
+	for (auto i = 0; i < 100; i++)
+	{
+		co_await coro_compute_exec(i);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	coro_start(coro_compute());
+	main_ioc.run();
+	return 0;
+}
