c55a89917ab62f7cbc9e6c10afce4dbfef1c08db
dr <microcaicai@gmail.com>
2024-10-03 01:22:08 +0800

    add Qt Demo.

---
+++ tests/testqt/testqt.cpp
@@ -0,0 +1,50 @@
+
+
+#include <iostream>
+#include "ucoro/awaitable.hpp"
+#include <QtCore>
+#include <QTimer>
+#include <QCoreApplication>
+
+ucoro::awaitable<int> coro_compute_int(int value)
+{
+	auto ret = co_await executor_awaitable<int>([value](auto handle) {
+		QTimer::singleShot(0, [value, handle = std::move(handle)]() mutable {
+			std::this_thread::sleep_for(std::chrono::seconds(0));
+			std::cout << value << " value\n";
+			handle(value * 100);
+		});
+	});
+
+	co_return (value + ret);
+}
+
+ucoro::awaitable<void> coro_compute_exec(int value)
+{
+	auto ret = co_await coro_compute_int(value);
+	std::cout << "return: " << ret << std::endl;
+	co_return;
+}
+
+ucoro::awaitable<void> coro_compute()
+{
+	auto x = co_await ucoro::local_storage;
+	std::cout << "local storage: " << std::any_cast<std::string>(x) << std::endl;
+
+	for (auto i = 0; i < 100; i++)
+	{
+		co_await coro_compute_exec(i);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	std::string str = "hello";
+
+	QCoreApplication app(argc, argv);
+
+	coro_start(coro_compute(), str);
+
+	app.exec();
+	return 0;
+}
