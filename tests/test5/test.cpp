23faea48b9dbb8d37a281b86bac3eb88a99ea9f5
Jackarain <jack.wgm@gmail.com>
2024-10-05 08:30:03 +0800

    Add test5 for exception

---
+++ tests/test5/test.cpp
@@ -0,0 +1,56 @@
+
+#include <boost/asio.hpp>
+#include <iostream>
+#include "ucoro/awaitable.hpp"
+
+boost::asio::io_context main_ioc;
+
+ucoro::awaitable<int> coro_compute_int(int value)
+{
+	auto ret = co_await executor_awaitable<int>([value](auto handle) {
+		main_ioc.post([value, handle = std::move(handle)]() mutable {
+			std::this_thread::sleep_for(std::chrono::seconds(0));
+			throw std::bad_alloc{};
+			std::cout << value << " value\n";
+			handle(value * 100);
+		});
+	});
+
+	co_return (value + ret);
+}
+
+ucoro::awaitable<void> coro_compute_exec(int value)
+{
+	try {
+		auto ret = co_await coro_compute_int(value);
+		std::cout << "return: " << ret << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "exception: " << e.what() << std::endl;
+	}
+
+	co_return;
+}
+
+ucoro::awaitable<void> coro_compute()
+{
+	auto x = co_await ucoro::local_storage;
+	std::cout << "local storage: " << std::any_cast<std::string>(x) << std::endl;
+
+	for (auto i = 0; i < 100; i++)
+	{
+		co_await coro_compute_exec(i);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	std::string str = "hello";
+
+	coro_start(coro_compute(), str);
+
+	main_ioc.run();
+
+	return 0;
+}
