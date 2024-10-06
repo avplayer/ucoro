0349cbb3d114f7332759b3defb8e4068113cdef8
microcai <microcaicai@gmail.com>
2024-10-06 19:05:02 +0800

    实现 co_await 一个 asio 协程

--- tests/test2/test2.cpp
+++ tests/test2/test2.cpp
@@ -1,9 +1,81 @@
 
 #include <boost/asio.hpp>
+#include <boost/asio/co_spawn.hpp>
 #include <iostream>
 #include "ucoro/awaitable.hpp"
 
 boost::asio::io_context main_ioc;
+
+template <typename T>
+struct awaitable_return_value
+{
+	T return_value;
+	T await_resume()
+	{
+		return return_value;
+	}
+};
+
+template <>
+struct awaitable_return_value<void>
+{
+	void await_resume()
+	{
+	}
+};
+
+
+template <typename T> requires (!std::is_same_v<T, void>)
+auto  transform_asio_awaitable(boost::asio::awaitable<T>&& asio_awaitable)
+{
+	struct asio_awaitable_awiter : public awaitable_return_value<T>
+	{
+		asio_awaitable_awiter(boost::asio::awaitable<T>&& asio_awaitable) : asio_awaitable(std::move(asio_awaitable)){};
+
+		constexpr auto await_ready() { return false; }
+
+		auto await_suspend(std::coroutine_handle<> continue_handle)
+		{
+			boost::asio::co_spawn(main_ioc, [this](auto continue_handle) -> boost::asio::awaitable<void>
+			{
+				if constexpr (!std::is_void_v<T>)
+				{
+					this->return_value = co_await std::move(asio_awaitable);
+				}
+				else
+				{
+					co_await std::move(asio_awaitable);
+				}
+
+				continue_handle.resume();
+				co_return ;
+			}(continue_handle), [](std::exception_ptr){});
+		}
+
+		boost::asio::awaitable<T> asio_awaitable;
+	};
+	return asio_awaitable_awiter { std::move(asio_awaitable) };
+}
+
+
+namespace ucoro
+{
+
+template <typename T>
+struct await_transformer<boost::asio::awaitable<T>>
+{
+	static auto await_transform(boost::asio::awaitable<T>&& awaitee)
+	{
+		return transform_asio_awaitable(std::move(awaitee));
+	}
+};
+
+}
+
+boost::asio::awaitable<int> asio_coro_test()
+{
+	co_return 10;
+}
 
 ucoro::awaitable<int> coro_compute_int(int value)
 {
@@ -15,7 +87,7 @@ ucoro::awaitable<int> coro_compute_int(int value)
 		});
 	});
 
-	co_return (value + ret);
+	co_return (value + ret + co_await asio_coro_test());
 }
 
 ucoro::awaitable<void> coro_compute_exec(int value)
