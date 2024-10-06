b46cbd789710425c1100b8e5e93a1bdd7e8c0564
microcai <microcaicai@gmail.com>
2024-10-06 21:12:00 +0800

    asio 里面也能等待 ucoro 的协程啦！

--- tests/test2/test2.cpp
+++ tests/test2/test2.cpp
@@ -62,9 +62,45 @@ struct ucoro::await_transformer<boost::asio::awaitable<T>>
 	}
 };
 
-boost::asio::awaitable<int> asio_coro_test()
+// boost::asio::awaitable<int> operator co_await(ucoro::awaitable<int>&&)
+// {
+// 	return []() ->boost::asio::awaitable<int> { co_return 0; }();
+// }
+
+ucoro::awaitable<int> called_from_asio_coro()
 {
 	co_return 10;
+}
+
+struct initiate_do_invoke_ucoro_awaitable
+{
+	template <typename T, typename Handler> requires (!std::is_void_v<T>)
+	void operator()(Handler&& handler, ucoro::awaitable<T> *ucoro_awaitable) const
+	{
+		auto executor = boost::asio::get_associated_executor(handler);
+
+
+		[handler = std::move(handler), ucoro_awaitable = std::move(*ucoro_awaitable)]() mutable -> ucoro::awaitable<void>
+		{
+			auto return_value = co_await std::move(ucoro_awaitable);
+
+			handler(boost::system::error_code(), return_value);
+			co_return;
+		}().detach();
+	}
+};
+
+template <typename T>
+auto ucoro_awaitable_to_asio_awaitable(ucoro::awaitable<T> && ucoro_awaitable)
+{
+	return boost::asio::async_initiate<decltype(boost::asio::use_awaitable),
+	void(boost::system::error_code, T)>(
+		initiate_do_invoke_ucoro_awaitable(), boost::asio::use_awaitable, &ucoro_awaitable);
+}
+
+boost::asio::awaitable<int> asio_coro_test()
+{
+	co_return co_await ucoro_awaitable_to_asio_awaitable(called_from_asio_coro());
 }
 
 ucoro::awaitable<int> coro_compute_int(int value)
