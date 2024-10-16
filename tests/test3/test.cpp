d5c819bdb7bffc36098580dfbb7e2a6e937db426
microcai <microcaicai@gmail.com>
2024-10-16 20:36:57 +0800

    删除 executor_awaitable
    由于 callback_awaitable 已经具备了自动兼容 executor 的机制。因此额外提供
    executor_awaitable 的意义没有了。删掉 executor_awaitable 之。
    
    另外，更新之前写 callback_awaitable 的时候还强制依赖 executor_awaitable
    的 test 代码。

--- tests/test3/test.cpp
+++ tests/test3/test.cpp
@@ -5,16 +5,10 @@
 int main(int argc, char **argv)
 {
 	using CallbackAwaiterType0 = ucoro::CallbackAwaiter<void, decltype([](auto h) {}) >;
-	using ExecutorAwaiterType0 = ucoro::ExecutorAwaiter<void, decltype([](auto h) {}) > ;
-
 	using CallbackAwaiterType1 = ucoro::CallbackAwaiter<int, decltype([](auto h) {}) > ;
-	using ExecutorAwaiterType1 = ucoro::ExecutorAwaiter<int, decltype([](auto h) {}) > ;
 
 	static_assert(ucoro::detail::is_awaiter_v < CallbackAwaiterType0 >, "not a coroutine");
-	static_assert(ucoro::detail::is_awaiter_v < ExecutorAwaiterType0 >, "not a coroutine");
-
 	static_assert(ucoro::detail::is_awaiter_v < CallbackAwaiterType1 >, "not a coroutine");
-	static_assert(ucoro::detail::is_awaiter_v < ExecutorAwaiterType1 >, "not a coroutine");
 
 	static_assert(ucoro::detail::is_awaiter_v < ucoro::awaitable<void> >, "not a coroutine");
 	static_assert(ucoro::detail::is_awaiter_v < ucoro::awaitable<int> >, "not a coroutine");
--- tests/test_curl/test_curl.cpp
+++ tests/test_curl/test_curl.cpp
@@ -5,17 +5,53 @@
 
 #include "curl/curl.h"
 
+struct completion_job_data
+{
+	static void do_completion_job(void* completion_data)
+	{
+		(*reinterpret_cast<completion_job_data*>(completion_data))();
+		delete reinterpret_cast<completion_job_data*>(completion_data);
+	}
+
+	virtual ~completion_job_data()
+	{
+	}
+
+	virtual void operator() ()
+	{
+	}
+};
+
+
+template<typename continuation_t>
+struct completion_job_data_for_coro : public completion_job_data
+{
+	continuation_t continuation;
+
+	completion_job_data_for_coro(continuation_t&& continuation)
+		: continuation(continuation)
+	{
+	}
+
+	virtual void operator() () override
+	{
+		continuation();
+	}
+};
+
 ucoro::awaitable<void> async_curl_http_post(std::string url)
 {
-	CURLM *curl = co_await ucoro::local_storage_t<CURLM *>{};
+	CURLM* curl = co_await ucoro::local_storage_t<CURLM*>{};
 
-	co_await executor_awaitable<void>([curl, url](std::coroutine_handle<void> continuation)
+	co_await callback_awaitable<void>([curl, url](auto continuation)
 	{
 		auto http_handle = curl_easy_init();
 
+		auto complete_op = new completion_job_data_for_coro<decltype(continuation)>(std::move(continuation));
+
 		/* set the options (I left out a few, you get the point anyway) */
 		curl_easy_setopt(http_handle, CURLOPT_URL, url.c_str());
-		curl_easy_setopt(http_handle, CURLOPT_PRIVATE, continuation.address());
+		curl_easy_setopt(http_handle, CURLOPT_PRIVATE, complete_op);
 		curl_multi_add_handle(curl, http_handle);
 		curl_multi_wakeup(curl);
 
@@ -57,7 +93,7 @@ ucoro::awaitable<void> coro_compute()
 	global_exit_loop = true;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 	curl_global_init(CURL_GLOBAL_ALL);
 	auto curl = curl_multi_init();
@@ -70,9 +106,9 @@ int main(int argc, char **argv)
 	// 注意这里，执行到这里的时候，上诉 NOTE 里的 2号位代码执行完毕，return 了。1号位的代码，尚未执行到。
 
 	int msgs_left = -1;
-	CURLMsg *msg;
+	CURLMsg* msg;
 
-	for(;;)
+	for (;;)
 	{
 		int still_alive = 1;
 		curl_multi_perform(curl, &still_alive);
@@ -82,14 +118,13 @@ int main(int argc, char **argv)
 		{
 			if (msg->msg == CURLMSG_DONE)
 			{
-				CURL *e = msg->easy_handle;
+				CURL* e = msg->easy_handle;
 				fprintf(stderr, "R: %d - %s\n", msg->data.result, curl_easy_strerror(msg->data.result));
-				void * coroutine_handle_address = nullptr;
-				curl_easy_getinfo(e, CURLINFO_PRIVATE, &coroutine_handle_address);
-				if (coroutine_handle_address)
+				void* completion_job_data = nullptr;
+				curl_easy_getinfo(e, CURLINFO_PRIVATE, &completion_job_data);
+				if (completion_job_data)
 				{
-					auto completion_handle = std::coroutine_handle<void>::from_address(coroutine_handle_address);
-					completion_handle();
+					completion_job_data::do_completion_job(completion_job_data);
 				}
 
 				curl_multi_remove_handle(curl, e);
@@ -102,7 +137,9 @@ int main(int argc, char **argv)
 		}
 
 		if (global_exit_loop)
+		{
 			break;
+		}
 
 		curl_multi_wait(curl, NULL, 0, 1000, NULL);
 	}
